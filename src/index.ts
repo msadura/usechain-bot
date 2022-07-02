956eccd822640e3f9be226ae8830c72b042f3ce0
Marek <epicode.office@gmail.com>
2022-07-02 15:32:00 +0200

    Remove redundant code

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { connectProvider } from '@app/blockchain/provider';
-import { testApeXTrade } from '@app/trade/apeX/testApeXTrade';
 import { activateArbitrumAccounts } from '@app/arbitrum/arbitrum';
-
-const PORT = process.env.PORT || 3002;
 
 async function main() {
   await connectProvider();
-
-  activateArbitrumAccounts();
-
+  // 1) Uncomment and run once to generate file with accounts
   // generateMinions(20);
-  // activateAccounts();
-  // uniswap();
-  // sushi();
-  // getGasPrice(getProvider());
 
-  // await connectWallet();
-  // const app = express();
-  // app.listen(PORT, () => {
-  //   console.log(`[ server ] ready on port ${PORT}`);
-  // });
-  // printAccountsWithSeeds(20, 21);
+  // 2) Send manually ETH to first minion account
+
+  // 3) Uncomment to active generated minions
+  activateArbitrumAccounts();
 }
 
 main();
